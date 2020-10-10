9bf3605015d7664618d28df2ace29a5b884f10dd
Brian Washburn <blueherongis@gmail.com>
2020-10-10 14:01:12 -0700

    Added ColorToHex component for convertting RGBA to hexidecimal format

---
+++ Heron/ColorToHex.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Heron
+{
+    public class ColorToHex : HeronComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the ColorToHex class.
+        /// </summary>
+        public ColorToHex()
+          : base("ColorToHex", "C2H",
+              "Convert an RGBA color to hexidecimal format.",
+              "GIS Tools")
+        {
+        }
+        public override Grasshopper.Kernel.GH_Exposure Exposure
+        {
+            get { return GH_Exposure.quarternary; }
+        }
+
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddColourParameter("colorRGBA", "colorRGBA", "RGBA color to convert to hexidecimal format", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("colorHexidecmial", "colorHex", "Hexidecimal color converted from RGBA", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Color color = Color.Empty;
+            DA.GetData<Color>(0, ref color);
+            string hex = String.Format("#{0:X2}{1:X2}{2:X2}{3:X2}", color.R, color.G, color.B, color.A);
+            DA.SetData(0, hex);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Properties.Resources.raster;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5fd79e04-c146-4b3e-9e96-b1c0ec66310f"); }
+        }
+    }
+}
