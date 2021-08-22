04c59576045742b1ee3b72297680369626f34fac
Brian Washburn <blueherongis@gmail.com>
2021-08-21 20:24:37 -0700

    Added LockBits Method, but GH_MemoryBitmap is as fast.

--- Heron/ImageCubeToEqui.cs
+++ Heron/ImageCubeToEqui.cs
@@ -44,22 +44,26 @@ namespace Heron
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddBooleanParameter("Converted", "C", "True if the cubemap was succesffuly converted to equirectangular.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Equirectangular File Location", "F", "File location of equiMap if successfully converted.", GH_ParamAccess.item);
         }
 
         public class SolveResults
         {
             public Boolean Converted { get; set; } 
+            public string EquiFileLoc { get; set; }
         }
 
         SolveResults Compute (string cubeMap, string equiMap, int w)
         {
             var rc = new SolveResults();
             Boolean converted = false;
+            string equiFileLoc = string.Empty;
   
             if (!File.Exists(cubeMap))
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Cubemap file path does not exist.");
                 rc.Converted = converted;
+                rc.EquiFileLoc = equiFileLoc;
                 return rc;
             }
 
@@ -67,6 +71,7 @@ namespace Heron
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Cubemap file path and equirectangular file path are the same. Cubemap not converted.");
                 rc.Converted = converted;
+                rc.EquiFileLoc = equiFileLoc;
                 return rc;
             }
 
@@ -95,6 +100,7 @@ namespace Heron
                         equi.Save(equiMap);
                         equi.Dispose();
                         bm.Dispose();
+                        equiFileLoc = equiMap;
                         converted = true;
                     }
                 }
@@ -105,6 +111,7 @@ namespace Heron
                 }
 
                 rc.Converted = converted;
+                rc.EquiFileLoc = equiFileLoc;
 
                 return rc;
             }
@@ -164,6 +171,7 @@ namespace Heron
             if (results != null)
             {
                 DA.SetData(0, results.Converted);
+                DA.SetData(1, results.EquiFileLoc);
             }
         }
 
@@ -198,10 +206,11 @@ namespace Heron
             //  empty bottom empty empty
 
             Bitmap equiTexture = new Bitmap(w, w / 2, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GH_MemoryBitmap ghEquiMap = new GH_MemoryBitmap(equiTexture);
 
             ///GH_MemoryBitmap Sample faster than GetPixel
             ///https://www.grasshopper3d.com/forum/topics/unsafe?page=1&commentId=2985220%3AComment%3A808291&x=1#2985220Comment808291
-            GH_MemoryBitmap sampler = new GH_MemoryBitmap(bm);
+            GH_MemoryBitmap ghBitmap = new GH_MemoryBitmap(bm);
 
             double u, v; //Normalised texture coordinates, from 0 to 1, starting at lower left corner
             double phi, theta; //Polar coordinates
@@ -325,15 +334,169 @@ namespace Heron
                         //throw new ArgumentNullException("x = " + xPixel.ToString() + " y = " + yPixel.ToString()+" i="+i+" j="+j);
                     }
 
-                    sampler.Sample(xPixel, yPixel, ref color);
-                    //color = bm.GetPixel(xPixel, yPixel);
+                    //sampler.Sample(xPixel, yPixel, ref color);
+                    color = ghBitmap.Colour(xPixel, yPixel);
 
-                    equiTexture.SetPixel(i, j, color);
+                    ghEquiMap.Colour(i, j, color);
 
                 }
             }
 
-            sampler.Release(false);
+            ghBitmap.Release(false);
+            ghEquiMap.Release(true);
+
+            return equiTexture;
+        }
+
+
+
+        public unsafe static Bitmap ConvertCubicToEquirectangularLockbits(Bitmap bm, int w)
+        {
+            //algorithm from https://stackoverflow.com/questions/34250742/converting-a-cubemap-into-equirectangular-panorama
+            //for cube maps with the following format:
+            //  empty top empty empty
+            //  left forward right backward
+            //  empty bottom empty empty
+
+            Bitmap equiTexture = new Bitmap(w, w / 2, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GH_MemoryBitmap ghEquiTexture = new GH_MemoryBitmap(equiTexture);
+
+            ///GH_MemoryBitmap Sample faster than GetPixel
+            ///https://www.grasshopper3d.com/forum/topics/unsafe?page=1&commentId=2985220%3AComment%3A808291&x=1#2985220Comment808291
+            GH_MemoryBitmap ghBitmap = new GH_MemoryBitmap(bm);
+
+            double u, v; //Normalised texture coordinates, from 0 to 1, starting at lower left corner
+            double phi, theta; //Polar coordinates
+            int cubeFaceWidth, cubeFaceHeight;
+
+
+            cubeFaceWidth = bm.Width / 4; //4 horizontal faces
+            cubeFaceHeight = bm.Height / 3; //3 vertical faces
+
+
+            for (int j = 0; j < equiTexture.Height; j++)
+            {
+                //Rows start from the bottom
+                v = 1 - ((double)j / equiTexture.Height);
+                theta = v * Math.PI;
+
+                for (int i = 0; i < equiTexture.Width; i++)
+                {
+                    //Columns start from the left
+                    u = ((double)i / equiTexture.Width);
+                    phi = u * 2 * Math.PI;
+
+                    double x, y, z; //Unit vector
+                    x = Math.Sin(phi) * Math.Sin(theta) * -1;
+                    y = Math.Cos(theta);
+                    z = Math.Cos(phi) * Math.Sin(theta) * -1;
+
+                    double xa, ya, za;
+                    double a;
+
+                    double[] maxArr = new double[3] { Math.Abs(x), Math.Abs(y), Math.Abs(z) };
+                    //a = Math.Max(new double[3] { Math.Abs(x), Math.Abs(y), Math.Abs(z) });
+                    a = maxArr.Max();
+
+                    //Vector Parallel to the unit vector that lies on one of the cube faces
+                    xa = x / a;
+                    ya = y / a;
+                    za = z / a;
+
+                    Color color = Color.Transparent;
+                    int xPixel, yPixel;
+                    int xOffset, yOffset;
+
+                    if (xa == 1)
+                    {
+                        //Right
+                        xPixel = (int)((((za + 1) / 2) - 1) * cubeFaceWidth);
+                        xOffset = 2 * cubeFaceWidth; //Offset
+                        yPixel = (int)((((ya + 1) / 2)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight; //Offset
+                    }
+                    else if (xa == -1)
+                    {
+                        //Left
+                        xPixel = (int)((((za + 1) / 2)) * cubeFaceWidth);
+                        xOffset = 0;
+                        yPixel = (int)((((ya + 1) / 2)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else if (ya == 1)
+                    {
+                        //Up
+                        xPixel = (int)((((xa + 1) / 2)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((za + 1) / 2) - 1) * cubeFaceHeight);
+                        yOffset = 2 * cubeFaceHeight;
+                    }
+                    else if (ya == -1)
+                    {
+                        //Down
+                        xPixel = (int)((((xa + 1) / 2)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((za + 1) / 2)) * cubeFaceHeight);
+                        yOffset = 0;
+                    }
+                    else if (za == 1)
+                    {
+                        //Front
+                        xPixel = (int)((((xa + 1) / 2)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((ya + 1) / 2)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else if (za == -1)
+                    {
+                        //Back
+                        xPixel = (int)((((xa + 1) / 2) - 1) * cubeFaceWidth);
+                        xOffset = 3 * cubeFaceWidth;
+                        yPixel = (int)((((ya + 1) / 2)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else
+                    {
+                        //Debug.LogWarning("Unknown face, something went wrong");
+                        xPixel = 0;
+                        yPixel = 0;
+                        xOffset = 0;
+                        yOffset = 0;
+                    }
+
+                    xPixel = Math.Abs(xPixel);
+                    yPixel = Math.Abs(yPixel);
+
+                    xPixel += xOffset;
+                    yPixel += yOffset;
+
+                    if (xPixel < 0 || yPixel < 0)
+                    {
+                        throw new ArgumentNullException("x = " + xPixel.ToString() + " y = " + yPixel.ToString());
+                    }
+
+                    if (xPixel > (bm.Width - 1))
+                    {
+                        xPixel = xPixel - 1;
+                        //throw new ArgumentNullException("x = " + xPixel.ToString() + " y = " + yPixel.ToString()+" i="+i+" j="+j);
+                    }
+
+                    if (yPixel > (bm.Height - 1))
+                    {
+                        yPixel = yPixel - 1;
+                        //throw new ArgumentNullException("x = " + xPixel.ToString() + " y = " + yPixel.ToString()+" i="+i+" j="+j);
+                    }
+
+                    //sampler.Sample(xPixel, yPixel, ref color);
+                    color = ghBitmap.Colour(xPixel, yPixel);
+
+                    ghEquiTexture.Colour(i, j, color);
+
+                }
+            }
+
+            ghBitmap.Release(false);
+            ghEquiTexture.Release(true);
 
             return equiTexture;
         }
