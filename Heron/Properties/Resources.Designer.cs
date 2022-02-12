909205537a52676f6a4971fbee6ca4abd7054264
Brian Washburn <blueherongis@gmail.com>
2022-02-12 13:29:32 -0800

    Embeded REST endpoints as fallback for lack of internet connection.

--- Heron/Properties/Resources.Designer.cs
+++ Heron/Properties/Resources.Designer.cs
@@ -121,6 +121,16 @@ namespace Heron.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] HeronServiceEndpoints {
+            get {
+                object obj = ResourceManager.GetObject("HeronServiceEndpoints", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap img {
