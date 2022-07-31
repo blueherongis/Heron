8ddf42f76aa1664084eaafd8fdc3947e233f4b93
Brian Washburn <blueherongis@gmail.com>
2022-07-31 11:55:37 -0700

    Force load Heron from disk

--- Heron/Properties/AssemblyInfo.cs
+++ Heron/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: GH_Loading(GH_LoadingDemand.ForceDirect)]
+//[assembly: GH_Loading(GH_LoadingDemand.ForceDirect)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
